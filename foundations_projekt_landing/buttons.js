4df9ad1b3c5b5d297e1b698a4e5f68a9eb3def4c
SonchessK <Sofiia.Koshova@westlotto.de>
2025-05-23 10:01:27 +0200

    lol

---
+++ foundations_projekt_landing/buttons.js
@@ -0,0 +1,21 @@
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll("button");
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    alert(button.id);
+  });
+});
+
+
+
+
+/**
+ * Some useful events include:
+click
+dblclick
+keydown
+keyup
+ */
